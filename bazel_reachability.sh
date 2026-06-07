05f5dd7deed12f457a4cf490c7864839ab42c09b
Jesse Schalken <me@jesseschalken.com>
2026-06-08 02:48:00 +1000

    Fix bazel_reachability.sh

--- bazel_reachability.sh
+++ bazel_reachability.sh
@@ -2,12 +2,12 @@
 
 set -eu -o pipefail
 
-METADATA_DIR=src/main/java/com/google/devtools/build/lib/bazel/graalvm_resources/META-INF/native-image/bazel/bazel
+METADATA_DIR=src/main/java/com/google/devtools/build/lib/bazel/nativeimage/META-INF/native-image/bazel/bazel
 AGENT_LIB=bazel-out/../../../external/+bazel_build_deps+graalvm_ce/lib/libnative-image-agent.dylib
 AGENT_ARG="-agentpath:${AGENT_LIB}=config-merge-dir=${METADATA_DIR}"
 BAZEL_ARGS=(--host_jvm_args="${AGENT_ARG}")
 
-bazel build //src:bazel-dev
+bazel build -c opt //src:bazel-dev
 ./bazel-bin/src/bazel-dev "${BAZEL_ARGS[@]}" "$@"
 ./bazel-bin/src/bazel-dev "${BAZEL_ARGS[@]}" shutdown
 
